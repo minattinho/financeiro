784507671737a9de373df01dd01dfb20e7527d4c
Gabriel Minatto Guedin <164443644+minattinho@users.noreply.github.com>
2025-04-08 15:55:00 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Link from "next/link";
-import { UserProvider, useUser } from "@/context/UserContext";
+import { UserProvider, useUser } from "../../context/UserContext";
 
 export const metadata = {
   title: "July - Assistente Financeiro",
