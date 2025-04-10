0f7f13e71d86f235072098c8be04fcdc2d5f4b59
Gabriel Minatto <gabrielminattoguedin@outlook.com>
2025-04-09 23:28:24 -0300

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { UserProvider } from "./context/UserContext";
-import Header from "./components/Header";
+import { Header } from "./components/header";
 
 export const metadata = {
   title: "July - Assistente Financeiro",
